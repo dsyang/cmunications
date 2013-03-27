c8bc34d940700cbf2f4995035bdb317fd92bd73b
Dan Yang <dsyang92@gmail.com>
2013-03-27 16:05:29 -0400

    app.js

---
+++ app.js
@@ -0,0 +1,42 @@
+var express = require("express");
+var mongo = require('mongodb');
+var app = express();
+var MongoClient = mongo.MongoClient;
+var connectionURI = process.env.MONGOLAB_URI ||
+    "mongodb://localhost:27017/myNotebook";
+var port = process.env.PORT || 8889;
+var db;
+
+app.use(express.bodyParser());
+
+
+app.get("/", function(request, response) {
+    var data = {};
+    defaultFunction(request, response, data);
+})
+
+
+function defaultFunction(request, response) {
+    response.send({"hello": "world!"});
+}
+
+module.exports = {
+    defaultFunction: defaultFunction
+};
+
+// Only runs if this file is passed into node "node app.js"
+if(__filename == process.argv[1]) {
+    MongoClient.connect(connectionURI, dbConnectCallback);
+
+    function dbConnectCallback(err, database) {
+        if(err) {
+            console.log("ERROR opening database:  "+err);
+        } else {
+            console.log("Database connection established.");
+            db = database;
+            app.listen(port);
+            console.log("Created server on port: "+port);
+        }
+    }
+
+}
